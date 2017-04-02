642b84c628f1f588737259d065071c86a4716eb5
alestainer <nikolaev.anton@phystech.edu>
2017-04-01 15:33:02 -1000

    TornToPieces changed -1 to 'route not found'

--- TornToPieces.py
+++ TornToPieces.py
@@ -2,9 +2,11 @@ import sys
 
 __author__ = 'alestainer'
 
+
 class Solution:
     visited = set()
     dictionary = dict()
+
     def find_a_path(self, start, end):
         """
         :rtype
@@ -14,14 +16,16 @@ class Solution:
             return start + " " + end
         elif (self.dictionary[start] <= self.visited):
             return -1
-        elif max(map(lambda x: Solution().find_a_path(start=x, end=end), list(self.dictionary[start] - self.visited))) == -1:
+        elif max(map(lambda x: Solution().find_a_path(start=x, end=end),
+                     list(self.dictionary[start] - self.visited))) == -1:
             return -1
         else:
-            return  start + " " + max(map(lambda x: Solution().find_a_path(start=x, end=end), self.dictionary[start]))
+            return start + " " + max(map(lambda x: Solution().find_a_path(start=x, end=end), self.dictionary[start]))
 
     def __init__(self, dictionary):
         self.visited = set()
         self.dictionary = dictionary
+
 
 if (__name__ == 'main.py'):
     vertexes = []
@@ -35,7 +39,6 @@ if (__name__ == 'main.py'):
         neighbors.append(set(stations[1:]))
     start, end = lines[-1]
     dictionary = dict(zip(vertexes, neighbors))
-    sys.stdout.write(Solution(dictionary=dictionary).find_a_path(start=start, end=end))
-
-
-
+    sys.stdout.write(Solution(dictionary=dictionary).find_a_path(start=start, end=end)
+                     if (
+    Solution(dictionary=dictionary).find_a_path(start=start, end=end) != -1) else 'route not found')
