00b632eee7354d5a6eea48041160256471b03c08
alestainer <nikolaev.anton@phystech.edu>
2017-11-13 07:18:36 -1000

    Fixed binary Search

--- utils/binary-search.py
+++ utils/binary-search.py
@@ -4,17 +4,21 @@ class BinarySearcher:
         """Returns result of searching number in the array.
         Array must be sorted
         first return says if the number was found, the second - gives the coordinates of the number or the closest to the number"""
-        pivot = len(array) / 2
-        if (array[pivot] > number and len(array) > 1):
-            return BinarySearcher.binary_search(array=array[:pivot], number=number)[0], \
-                   BinarySearcher.binary_search(array=array[:pivot], number=number)[1]
-        elif (array[pivot] < number and len(array) > 1):
-            return BinarySearcher.binary_search(array=array[pivot:], number=number)[0], \
-                   BinarySearcher.binary_search(array=array[pivot:], number=number)[1] + pivot
-        elif (array[pivot] == number):
-            return True, pivot
-        else:
-            return False, pivot
+        def bin_search(array, l, r, number):
+        	if r >= l:
 
+        		m = l + (r - l) / 2
+
+        		if array[m] == number:
+        			return m
+
+	        	elif array[m] > number:
+    	    		return bin_search(array, l, m - 1, number)
+
+	        	else:
+    	    		return bin_search(array, m + 1, number)
+
+    	    else:
+    	    	return r
 
 print(BinarySearcher.binary_search([1, 2, 3], 3))
